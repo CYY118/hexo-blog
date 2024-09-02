024ba9ed001127b54e206fa68ced6be89e5fbfdb
CYY118 <84430109+CYY118@users.noreply.github.com>
2024-09-02 20:18:39 +0800

    更新 fomal.js

--- public/js/fomal.js
+++ public/js/fomal.js
@@ -2789,7 +2789,7 @@ function createtime() {
   var dis = Math.trunc(23400000000 + ((now - start) / 1000) * 17); // 距离=秒数*速度 记住转换毫秒
   var unit = (dis / 149600000).toFixed(6);  // 天文单位
   // 网站诞生时间
-  var grt = new Date("08/09/2022 00:00:00");
+  var grt = new Date("09/01/2024 00:00:00");
   var days = (now - grt) / 1e3 / 60 / 60 / 24,
     dnum = Math.floor(days),
     hours = (now - grt) / 1e3 / 60 / 60 - 24 * dnum,
